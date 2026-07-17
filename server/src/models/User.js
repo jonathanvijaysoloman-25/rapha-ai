b17ac9f488dc40b8d8af1031a6bfbff015644504
jonathanvijaysoloman-25 <jonathanvijaysoloman@gmail.com>
2026-07-17 10:40:54 +0530

    Fix User model and auth updates

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,15 +1,50 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    age: {
+      type: Number,
+    },
+
+    gender: {
+      type: String,
+    },
+
+    height: {
+      type: Number,
+    },
+
+    weight: {
+      type: Number,
+    },
+
+    bloodGroup: {
+      type: String,
+    },
+
+    medicalHistory: {
+      type: String,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-}, { timestamps: true });
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
